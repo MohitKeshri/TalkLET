968036dbef9c63bb613ba61a717e1b904eb5e57f
Mohit Keshri <sendtomohit99@gmail.com>
2025-04-25 23:30:55 +0530

    basic ui done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import SearchInput from "./components/sidebar/SearchInput";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+
 
 function App() {
-  return (<>
-    HELLO
-  </>);
+  return (
+   <div className="p-4 h-screen flex items-center justify-center"> 
+      <Home/>
+   </div>
+  );
 }
 
 export default App;
